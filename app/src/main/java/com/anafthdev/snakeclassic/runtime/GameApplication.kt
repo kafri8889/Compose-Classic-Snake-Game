2b1e5070497a69975889bf83ff1b2d67c1c8781e
kafri8889 <kafri8889@gmail.com>
2022-12-23 20:50:15 +0700

    add `@HiltAndroidApp` annotation

--- app/src/main/java/com/anafthdev/snakeclassic/runtime/GameApplication.kt
+++ app/src/main/java/com/anafthdev/snakeclassic/runtime/GameApplication.kt
@@ -2,8 +2,10 @@ package com.anafthdev.snakeclassic.runtime
 
 import android.app.Application
 import com.anafthdev.snakeclassic.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class GameApplication: Application() {
 	
 	override fun onCreate() {
