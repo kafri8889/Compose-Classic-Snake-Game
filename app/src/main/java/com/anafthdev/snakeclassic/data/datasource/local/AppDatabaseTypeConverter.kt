5ffdeba74bb2abd4a4aaa133f5e65fe0facd4ddf
kafri8889 <kafri8889@gmail.com>
2022-12-26 11:48:20 +0700

    fix type converter

--- app/src/main/java/com/anafthdev/snakeclassic/data/datasource/local/AppDatabaseTypeConverter.kt
+++ app/src/main/java/com/anafthdev/snakeclassic/data/datasource/local/AppDatabaseTypeConverter.kt
@@ -1,15 +1,15 @@
 package com.anafthdev.snakeclassic.data.datasource.local
 
 import androidx.room.TypeConverter
-import com.anafthdev.snakeclassic.common.GameConfiguration
+import com.anafthdev.snakeclassic.model.GameConfigurationData
 import com.google.gson.Gson
 
 object AppDatabaseTypeConverter {
 	
 	@TypeConverter
-	fun gameConfigurationDataToJson(gameConfiguration: GameConfiguration) = Gson().toJson(gameConfiguration)
+	fun gameConfigurationDataToJson(gameConfiguration: GameConfigurationData) = Gson().toJson(gameConfiguration)
 	
 	@TypeConverter
-	fun gameConfigurationDataFromJson(json: String) = Gson().fromJson(json, GameConfiguration::class.java)
+	fun gameConfigurationDataFromJson(json: String) = Gson().fromJson(json, GameConfigurationData::class.java)
 	
 }
