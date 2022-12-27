fb4c79977c40bb07f016aa280ef131798ac96069
kafri8889 <kafri8889@gmail.com>
2022-12-27 22:55:33 +0700

    Add navigation bar padding

--- app/src/main/java/com/anafthdev/snakeclassic/ui/score/ScoreScreen.kt
+++ app/src/main/java/com/anafthdev/snakeclassic/ui/score/ScoreScreen.kt
@@ -1,9 +1,7 @@
 package com.anafthdev.snakeclassic.ui.score
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
@@ -110,6 +108,10 @@ fun ScoreScreen(
 					score = score
 				)
 			}
+		}
+		
+		item {
+			Spacer(modifier = Modifier.navigationBarsPadding())
 		}
 	}
 }
